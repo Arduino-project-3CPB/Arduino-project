77b0b6c251b08d7d947fa34da6c8a1a5f58b993e
adszzz11 <54319448+adszzz11@users.noreply.github.com>
2021-09-08 11:47:58 +0900

    Create arduino_yeji.ino
    main / arduino should-code init

---
+++ arduino_yeji.ino
@@ -0,0 +1,32 @@
+int L1=A0;
+int L2=A1;
+int R1=A2;
+int R2=A3;
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+
+  //일단 출발
+
+  if(R2<100) {
+    if(R1<100) {
+      //왼쪽 반대로 돌리고 오른쪽 존나 가
+    }
+    else {
+      //왼쪽 정지하고 오른쪽 존나 가
+    }
+  }
+  else if(L2<100) {
+    if(L1<100) {
+      오른쪽 반대로 돌리고 왼쪽 존나 가
+    }
+    else {
+      //오른쪽 정지하고 왼쪽 존나 가
+    }
+  }
+}
