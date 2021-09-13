d8c61bece53c0b85ae661a8cba52cba0891d4b84
adszzz11 <54319448+adszzz11@users.noreply.github.com>
2021-09-13 16:17:29 +0900

    main / line-tracer complete-1

--- arduino_yeji/arduino_yeji.ino
+++ arduino_yeji/arduino_yeji.ino
@@ -1,11 +1,6 @@
 #include <AFMotor.h>
 
 
-//센서 인식부, ShouldCode 상태
-//int L1=A0;
-//int L2=A1;
-//int R1=A2;
-//int R2=A3;
 
 AF_DCMotor leftMotor(4);
 AF_DCMotor rightMotor(3);
@@ -17,6 +12,12 @@ void setup() {
   //init
   Serial.begin(9600);
   Serial.println("Motor Start");
+  delay(500);
+
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
   
   //leftMotor 구동부
   leftMotor.setSpeed(255);
@@ -29,13 +30,47 @@ void setup() {
 }
 
 void loop() {
+
+  int L1=digitalRead(A0); // 맨 왼쪽
+  int L2=digitalRead(A1); // 왼쪽 중간
+  int R2=digitalRead(A3); // 오른쪽 중간
+  int R1=digitalRead(A2); //맨 오른쪽
+
+  //노란색이 오른쪽 끝 A2
+  //검은색이 오른쪽 중간 A3
+  //A1 왼쪽 중간
+  //A0 왼쪽 끝
+
+  
   //뒤집은 거 기준으로 왼쪽 모터 구동
   leftMotor.run(FORWARD);
   //뒤집은 거 기준으로 오른쪽 모터 구동
   rightMotor.run(BACKWARD);
 
+  
+  if(R2==0) {
+    if(R1==0) {
+      rightMotor.run(FORWARD);
+      leftMotor.run(BACKWARD);
+    }
+    else {
+     leftMotor.run(FORWARD);
+     rightMotor.run(RELEASE);
+    }
+  }
 
-
+  
+  if(L2==0) {
+    if(L1==0) {
+      leftMotor.run(BACKWARD);
+      leftMotor.run(BACKWARD);
+      rightMotor.run(BACKWARD);
+    }
+    else {
+      leftMotor.run(RELEASE);
+      rightMotor.run(BACKWARD);
+    }
+  }
 
 
 
