9323263ea2f7753225ae4e79e3c0d31f60a40d99
adszzz11 <54319448+adszzz11@users.noreply.github.com>
2021-09-28 21:52:47 +0900

    main / arduino line tracing

--- arduino_yeji/arduino_yeji.ino
+++ arduino_yeji/arduino_yeji.ino
@@ -2,10 +2,12 @@
 
 
 
-AF_DCMotor leftMotor(4);
-AF_DCMotor rightMotor(3);
+AF_DCMotor leftMotor(1);
+AF_DCMotor rightMotor(2);
 
 
+int rightSpeed;
+int leftSpeed;
 
 void setup() {
   
@@ -18,28 +20,38 @@ void setup() {
   pinMode(A1, INPUT);
   pinMode(A2, INPUT);
   pinMode(A3, INPUT);
-  
+
+  rightSpeed=250;
+  leftSpeed=250;
   //leftMotor 구동부
-  leftMotor.setSpeed(255);
-  leftMotor.run(RELEASE);
+  leftMotor.setSpeed(leftSpeed);
+//  leftMotor.run(RELEASE);
 
   //rightMotor 구동부
-  rightMotor.setSpeed(255);
-  rightMotor.run(RELEASE);
+  rightMotor.setSpeed(rightSpeed);
+//  rightMotor.run(RELEASE);
   
 }
-
 void loop() {
 
-  int L1=digitalRead(A0); // 맨 왼쪽
-  int L2=digitalRead(A1); // 왼쪽 중간
-  int R2=digitalRead(A3); // 오른쪽 중간
-  int R1=digitalRead(A2); //맨 오른쪽
-
+  int L1=analogRead(A0); // 맨 왼쪽
+  int L2=analogRead(A1); // 왼쪽 중간
+  int R2=analogRead(A3); // 오른쪽 중간
+  int R1=analogRead(A2); //맨 오른쪽
+  Serial.print("L1 : ");
+  Serial.print(L1);
+  Serial.print(", L2 : ");
+  Serial.print(L2);
+  Serial.print(", R1 : ");
+  Serial.print(R1);
+  Serial.print("R2 : ");
+  Serial.println(R2);
   //노란색이 오른쪽 끝 A2
   //검은색이 오른쪽 중간 A3
   //A1 왼쪽 중간
   //A0 왼쪽 끝
+  leftMotor.setSpeed(leftSpeed);
+  rightMotor.setSpeed(rightSpeed);
 
   
   //뒤집은 거 기준으로 왼쪽 모터 구동
@@ -47,30 +59,44 @@ void loop() {
   //뒤집은 거 기준으로 오른쪽 모터 구동
   rightMotor.run(BACKWARD);
 
-  
-  if(R2==0) {
-    if(R1==0) {
-      rightMotor.run(FORWARD);
-      leftMotor.run(BACKWARD);
-    }
-    else {
-     leftMotor.run(FORWARD);
-     rightMotor.run(RELEASE);
-    }
+  if(L2>500) {
+//    rightMotor.run(RELEASE);
+//    leftMotor.setSpeed(leftSpeed);
+    rightMotor.setSpeed(rightSpeed-250);
+  }
+  if(R2>500) {
+//    leftMotor.run(RELEASE);
+//    rightMotor.setSpeed(rightSpeed);
+    leftMotor.setSpeed(leftSpeed-250);
   }
 
-  
-  if(L2==0) {
-    if(L1==0) {
-      leftMotor.run(BACKWARD);
-      leftMotor.run(BACKWARD);
-      rightMotor.run(BACKWARD);
-    }
-    else {
-      leftMotor.run(RELEASE);
-      rightMotor.run(BACKWARD);
-    }
-  }
+
+
+
+  //이게 각도 트는거
+//  if(R2>500) {
+//    if(R1>500) {
+//      rightMotor.run(FORWARD);
+//      leftMotor.run(BACKWARD);
+//    }
+//    else {
+//     leftMotor.run(FORWARD);
+//     rightMotor.run(RELEASE);
+//    }
+//  }
+//
+//  
+//  if(L2>500) {
+//    if(L1>500) {
+//      leftMotor.run(BACKWARD);
+//      leftMotor.run(BACKWARD);
+//      rightMotor.run(BACKWARD);
+//    }
+//    else {
+//      leftMotor.run(RELEASE);
+//      rightMotor.run(BACKWARD);
+//    }
+//  }
 
 
 
